c9800ec14c811226acdab11a4099b17a52ef56c2
Md Khalilul Rahman <105421454+Ashif2409@users.noreply.github.com>
2024-04-23 16:27:05 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -322,5 +322,3 @@ main().catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(port, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
